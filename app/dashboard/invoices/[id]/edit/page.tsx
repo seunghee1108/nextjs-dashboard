e5734e719ee14980d3f9f162556ed9e386c0df9b
승희 <bangseunghee1108@gmail.com>
2024-01-03 21:08:24 +0900

    notFound 함수를 사용하여 오류처리

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,15 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, xseunghee@bangseunghuiui-MacBookAir nextjs-dashboard % git add app/dashboard/invoices/id/eidt/pagre.ts 
+warning: could not open directory 'app/dashboard/invoices/id/eidt/': No such file or directory
+fatal: pathspec 'app/dashboard/invoices/id/eidt/pagre.ts' did not match any filesseunghee@bangseunghuiui-MacBookAir nextjs-dashboard % git add app/dashboard/invoices/id/eidt/pagre.ts 
+warning: could not open directory 'app/dashboard/invoices/id/eidt/': No such file or directory
+fatal: pathspec 'app/dashboard/invoices/id/eidt/pagre.ts' did not match any filesseunghee@bangseunghuiui-MacBookAir nextjs-dashboard % git add app/dashboard/invoices/id/eidt/pagre.ts 
+warning: could not open directory 'app/dashboard/invoices/id/eidt/': No such file or directory
+fatal: pathspec 'app/dashboard/invoices/id/eidt/pagre.ts' did not match any filesseunghee@bangseunghuiui-MacBookAir nextjs-dashboard % git add app/dashboard/invoices/id/eidt/pagre.ts 
+warning: could not open directory 'app/dashboard/invoices/id/eidt/': No such file or directory
+fatal: pathspec 'app/dashboard/invoices/id/eidt/pagre.ts' did not match any filesfetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page({ params }: { params: { id: string } }) {
   const id = params.id;
@@ -8,6 +17,11 @@ export default async function Page({ params }: { params: { id: string } }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+  
+  if (!invoice) {
+    notFound();
+  }
+
   return (
     <main>
       <Breadcrumbs
