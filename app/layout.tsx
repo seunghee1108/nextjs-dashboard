9113234128a53f96c4441af14f3328776f134955
bangseunghee <bangseunghee1108@gmail.com>
2024-01-09 22:29:53 +0900

    metadata객체 포함

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({  // 이웃 자식들에게 싹 다 적용시켜주겠다.
   children,
